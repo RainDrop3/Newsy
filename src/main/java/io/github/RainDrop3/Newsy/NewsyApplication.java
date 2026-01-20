c2696ce5100b694a6a9ce37c56367e868cdf6dae
kang <kanghy6220@gmail.com>
2026-01-20 19:28:59 +0900

    Initial: Project Setup

---
+++ src/main/java/io/github/RainDrop3/Newsy/NewsyApplication.java
@@ -0,0 +1,13 @@
+package io.github.RainDrop3.Newsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/RainDrop3/Newsy/NewsyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.RainDrop3.Newsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
